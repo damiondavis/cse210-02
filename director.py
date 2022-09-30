ce1ef4323d03e98c11a668b566a0bba0e488c696
ZuluDelta117 <97050214+ZuluDelta117@users.noreply.github.com>
2022-09-29 19:45:38 -0600

    Create director.py

---
+++ director.py
@@ -0,0 +1,10 @@
+from card import Card
+"""
+This section of code will have the bulk of the hilo game code in it. It will take the user input, pick the random card, update the score, 
+and control the output of the score.
+"""
+class Director:
+  pass
+
+
+  

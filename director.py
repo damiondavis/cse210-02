6caafe0ec767ec4f894b79ccd57ef2918190f137
ZuluDelta117 <97050214+ZuluDelta117@users.noreply.github.com>
2022-09-29 19:48:39 -0600

    Update director.py

--- director.py
+++ director.py
@@ -1,4 +1,4 @@
-from card import Card
+from hilo import Hilo
 """
 This section of code will have the bulk of the hilo game code in it. It will take the user input, pick the random card, update the score, 
 and control the output of the score.

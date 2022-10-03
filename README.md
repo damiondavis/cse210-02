517bf0096e999bbc5af7df956e4a0aff57a02a95
ZuluDelta117 <97050214+ZuluDelta117@users.noreply.github.com>
2022-10-03 12:36:11 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ See how long you can last!
 Make sure you have Python 3.8.0 or newer installed and running on your machine. Open a terminal and 
 browse to the project's root folder. Start the program by running the following command.
 ```
-python3 hilo 
+python3 cse210-02 
 ```
 You can also run the program from an IDE like Visual Studio Code. Start your IDE and open the 
 project folder. Select the main module inside the dice folder and click the "run" button.
@@ -33,3 +33,5 @@ root                    (project root folder)
 ## Authors
 ---
 * Zack Doxey (zack13@byui.edu)
+* Jordan Waite (jman1819@byui.edu)
+* Erick Nava (ericknava@byui.edu)
---
+++ game/hilo.py
@@ -0,0 +1,26 @@
+import random
+"""
+To create the deck of cards the player will be drawing from and to calculae the points.
+"""
+class Hilo:
+  
+  def __int__(self):
+    # self.correct is to keep track if the users input is correct or not and will be changed in another file
+    #self.correct = True
+    # needed to create the variable so that we can have the variable have a random number inserted into it
+    #self.value = 0
+    # set the points as zero even though they will start with 300 points so that the portion of code will be in another file
+    self.points = 0
+
+  def draw(self):
+    
+    # If I understand random.randint correctly this will randomly choose a number between 1 and 13
+    self.value = random.randint(1, 13)
+    # I am not sure if the score system will work yet. I am questioning the else statment because it is negative, but I hope it works
+    # I updated this statement so that it functions properly (I think), we don't need an if not statement here - Jordan
+
+    #I think I will be clearer to have an if in do update function (ENH)
+"""     if self.correct:
+      self.points = 100 
+    else:
+      self.points = -75 """
